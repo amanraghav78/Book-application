05d11db702232ab7cc4136663728b0bd97e6ad61
Aman <amanraghav682@gmail.com>
2024-07-08 16:47:27 +0530

    Implemented inmemory CRUD app

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
 const express= require('express')
 
-const app= new express();
+const app= express();
+
+app.use(express.json())
 
 const PORT = 3000;
+
+let books= [];
+
+//add a book
+app.post('/books', (req, res) => {
+    const { title, author } = req.body;
+
+    if(!title || !author ){
+        res.status(400).send("Missing title or author");
+    }
+
+    else{
+        const newBook= {id: books.length + 1, title, author};
+
+        books.push(newBook);
+        res.status(200).send(newBook);
+    }
+})
+
+//get all the books
+app.get('/books', (req, res)=>{
+    res.send(books);
+})
+
+//get a single book
+app.get('/books/:id', (req, res) => {
+    const book= books.find((b) => b.id === parseInt(req.params.id));
+    if(!book){
+        return res.status(404).send('Book not found');
+    }
+
+    res.json(book);
+})
 
 app.get('/', (req, res) => {
     res.send("Hello world! Making my first project myself!");
