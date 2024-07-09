4964acae8b0a4b53aed70e70c6f023b7365dd6f8
Aman <amanraghav682@gmail.com>
2024-07-09 23:17:38 +0530

    added mongo

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const express= require('express')
+const express = require('express')
+const mongoose = require('mongoose')
+const Book = require('./book');
+
+require('dotenv').config();
 
 const app= express();
 
@@ -6,26 +10,59 @@ app.use(express.json())
 
 const PORT = 3000;
 
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
 let books= [];
 
 //add a book
-app.post('/books', (req, res) => {
+app.post('/books', async (req, res) => {
     const { title, author } = req.body;
 
     if(!title || !author ){
-        res.status(400).send("Missing title or author");
+        return res.status(400).send("Missing title or author");
     }
 
-    else{
-        const newBook= {id: books.length + 1, title, author};
+    let book = new Book({ title: title, author: author});
+    book= await book.save();
+    res.send(book);
+})
 
-        books.push(newBook);
-        res.status(200).send(newBook);
+//update the book
+app.put('/books/:id', (req, res) => {
+    const { title, author } = req.body;
+
+    const book= books.find((b) => b.id === parseInt(req.params.id));
+
+    if(!book){
+        return res.status(401).send('Book not found');
     }
+
+    book.title= title || book.title;
+    book.author= author || book.author;
+
+    res.send(book);
+
+})
+
+//deleting a book
+app.delete('/books/:id', (req, res) => {
+    const bookIndex= books.findIndex((b) => b.id === parseInt (req.params.id));
+
+    if(bookIndex === -1){
+        return res.status(401).send('Book not found!');
+    }
+
+    books.splice(bookIndex, 1);
+    res.status(204).send('Book deleted!');
 })
 
 //get all the books
-app.get('/books', (req, res)=>{
+app.get('/books', async (req, res)=>{
+    const books = await Book.find();
     res.send(books);
 })
 
